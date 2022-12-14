68ae8ae5b72ab10c7018793fbdb0c92cf66b5d77
KUGAN P A <119141501+Kugan4343@users.noreply.github.com>
2022-12-14 10:39:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let salary=26000;
 let cibilvalue=1100;
-if(salary<25000,cibilvalue<1000){
+if(salary<25000&cibilvalue<1000){
     console.log('You are eligibile to take loan');
 }else{
     console.log('You are not eligible');
